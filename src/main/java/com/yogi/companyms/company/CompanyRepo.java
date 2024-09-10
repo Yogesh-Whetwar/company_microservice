50731bb07e6e8de0551dfa5364099a29dbde91b0
yogesh <yogeshwhetwar@gmail.com>
2024-09-10 14:51:17 +0530

    company microservice

---
+++ src/main/java/com/yogi/companyms/company/CompanyRepo.java
@@ -0,0 +1,7 @@
+package com.yogi.companyms.company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepo extends JpaRepository<Company,Long> {
+
+}
---
+++ src/main/java/com/yogi/companyms/company/impl/CompanyServiceImpl.java
@@ -0,0 +1,71 @@
+package com.yogi.companyms.company.impl;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yogi.companyms.company.Company;
+import com.yogi.companyms.company.CompanyRepo;
+import com.yogi.companyms.company.CompanyService;
+
+@Service
+public class CompanyServiceImpl  implements CompanyService{
+       @Autowired
+       CompanyRepo companyRepo;
+
+    @Override
+    public List<Company> getAllCompanies() {
+         return companyRepo.findAll();
+    }
+
+    @Override
+    public boolean updateCompany(Company company,Long id) { 
+         Optional<Company>c=companyRepo.findById(id);
+        if(c.isPresent()){
+            Company toUpdate=c.get();
+            toUpdate.setDescription(company.getDescription());
+            toUpdate.setName(company.getName());
+
+            companyRepo.save(toUpdate);
+            return true;
+        }
+        return false;
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'updateCompany'");
+    }
+
+    @Override
+    public void addCompany(Company company) { 
+        companyRepo.save(company);
+        
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'addCompany'");
+    }
+
+    @Override
+    public Company getCompanyById(Long id) {
+        Optional<Company>c=companyRepo.findById(id);
+        if(c.isPresent()){
+            Company toUpdate=c.get();
+            return toUpdate;
+        }
+        return null;
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'getCompanyById'");
+    }
+
+    @Override
+    public boolean deleteCompanyById(Long id) { 
+        Optional<Company>c=companyRepo.findById(id);
+        if(c.isPresent()){
+            Company toUpdate=c.get();
+            companyRepo.delete(toUpdate);
+            return true;
+        }
+        return false;
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'deleteCompanyById'");
+    }
+    
+}
